8929f324638a7889926fa8b8524f7669cbd6d7f4
shrey bhardwaj <21uec125@gmail.com>
2023-12-02 02:00:56 +0530

    final

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import MainNav from "./MainNav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
+import Image from "next/image";
 
 export const revalidate = 0;
 
@@ -14,7 +15,7 @@ const Navbar = async ({ session }) => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href={"/"} className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">{"Amul"}</p>
+            <Image src={"/amul.png"} height={80} width={80} alt="Amul" />
           </Link>
           <MainNav data={categories} />
           <NavbarActions session={session} />
